1bfb86b630b50fb9a5ad9ac8e99d53c4c4c49044
Anas Tariq <PURE-DEV-ANASTA@Anass-MacBook-Pro-M2.local>
2024-01-01 21:38:50 +0500

    fix: added handling for latest android version and added test for app module

---
+++ app/src/main/java/com/jhowcs/multimodulecoverage/AppCalculator.kt
@@ -0,0 +1,12 @@
+package com.jhowcs.multimodulecoverage
+
+class AppCalculator {
+
+    fun sum(n1: Int, n2: Int) = n1 + n2
+
+    fun multiply(n1: Int, n2: Int) = n1 * n2
+
+    fun divide(n1: Int, n2: Int) = n1 / n2
+
+    fun subtract(n1: Int, n2: Int) = n1 - n2
+}
